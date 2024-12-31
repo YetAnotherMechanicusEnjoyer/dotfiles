5aaf65edc04ad0c85b00c6e801736b789e0f3e7c
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2024-12-31 14:39:15 +0100

    [ADD] tranparency for NeoTree

--- nvim/lua/plugins/transparent.lua
+++ nvim/lua/plugins/transparent.lua
@@ -3,7 +3,7 @@ require("transparent").setup({
     'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
     'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer', 'NeoTreeNormal', 'NeoTreeNormalNC',
   },
   extra_groups = {}, -- table: additional groups that should be cleared
   exclude_groups = {}, -- table: groups you don't want to clear
