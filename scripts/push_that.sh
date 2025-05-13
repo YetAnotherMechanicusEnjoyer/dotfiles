e14c535fe2d326cca23e5e09f525a574e6d650ed
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-05-13 08:41:46 +0200

    [UPDATE] push_that.sh now uses gum

--- scripts/push_that.sh
+++ scripts/push_that.sh
@@ -1,2 +1,7 @@
-echo "use lazygit like a real chad"
-lazygit
+# Commit
+TYPE=$(gum choose "ADD" "EDIT" "FIX" "REMOVE" "UPDATE" "MERGE")
+TITLE=$(gum input --value "[$TYPE] " --placeholder "Title")
+DESCRIPTION=$(gum write --placeholder "Commit description")
+
+echo -e "\e[1m\e[92mTitle : $TITLE\nDescription : $DESCRIPTION"
+gum confirm "Commit changes ?" && git commit -m "$TITLE" -m "$DESCRIPTION"
