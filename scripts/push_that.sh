81c8da621d9773382d1526a0305b9673b08c6658
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-05-13 08:59:15 +0200

    [ADD] git push choice to push_that.sh && [EDIT] ANSI

--- scripts/push_that.sh
+++ scripts/push_that.sh
@@ -3,5 +3,14 @@ TYPE=$(gum choose "ADD" "EDIT" "FIX" "REMOVE" "UPDATE" "MERGE")
 TITLE=$(gum input --value "[$TYPE] " --placeholder "Title")
 DESCRIPTION=$(gum write --placeholder "Commit description")
 
-echo -e "\e[1m\e[92mTitle : $TITLE\nDescription : $DESCRIPTION"
+if [ "$DESCRIPTION" == "" ]; then
+  DESC="<no description>"
+else
+  DESC="$DESCRIPTION"
+fi
+
+echo -e "\e[1m\e[95m\e[4mTitle :\e[0m \e[1m\e[97m$TITLE\n\e[4m\e[95mDescription :\e[0m \e[90m\e[1m$DESC\e[0m"
+
 gum confirm "Commit changes ?" && git commit -m "$TITLE" -m "$DESCRIPTION"
+
+gum confirm "Push commits ?" && git push
