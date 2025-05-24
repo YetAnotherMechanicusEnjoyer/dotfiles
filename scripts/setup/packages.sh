3c8a66631e75e370d068c14856530db8449f6f82
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-05-24 17:05:35 +0200

    [ADD] docker group usermod in packages.sh

--- scripts/setup/packages.sh
+++ scripts/setup/packages.sh
@@ -30,5 +30,7 @@ sudo systemctl enable sshd
 sudo systemctl start docker
 sudo systemctl enable docker
 
+sudo usermod -a -G docker "$USER"
+
 sudo chsh -s /usr/bin/zsh "$USER"
 gum log --structured --level warn "Restart to change your default shell" shell "zsh"
