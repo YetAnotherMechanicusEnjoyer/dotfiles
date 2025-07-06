3957c5e23f7f39c05764ea318d8a857566e4dceb
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-06 21:16:49 +0200

    [UPDATE] setup.sh
    [ADD] ~/YAME/ folder

--- setup.sh
+++ setup.sh
@@ -1,21 +1,23 @@
 sudo pacman -Syu --needed gum
+mkdir -p ~/YAME/
+cp ./* ~/YAME/
 
 echo -e "\n\e[1m\e[96mDo you want to install packages ?\e[0m"
-gum confirm && sh ./scripts/setup/packages.sh
+gum confirm && sh ~/YAME/scripts/setup/packages.sh
 
 echo -e "\n\e[1m\e[96mDo you want to install yay ?\e[0m"
-gum confirm && sh ./scripts/setup/yay.sh
+gum confirm && sh ~/YAME/scripts/setup/yay.sh
 
 echo -e "\n\e[1m\e[96mDo you want to configure git ?\e[0m"
-gum confirm && sh ./scripts/setup/git.sh
+gum confirm && sh ~/YAME/scripts/setup/git.sh
 
 echo -e "\n\e[1m\e[96mDo you want to clone some repos ?\e[0m"
-gum confirm && sh ./scripts/setup/repo.sh
+gum confirm && sh ~/YAME/scripts/setup/repo.sh
 
 echo -e "\n\e[1m\e[96mDo you want to install Lxtharia's minegrub theme ?\e[0m"
-gum confirm && sudo sh ./grub/install_theme.sh
+gum confirm && sudo sh ~/YAME/scripts/setup/grub.sh
 
 echo -e "\n\e[1m\e[96mDo you want to install ML4W's hyprland dotfiles ?\e[0m"
-gum confirm && sh ./scripts/setup/hyprland_install.sh
+gum confirm && sh ~/YAME/scripts/setup/hyprland_install.sh
 
-sh ./link.sh
+sh ~/YAME/link.sh
