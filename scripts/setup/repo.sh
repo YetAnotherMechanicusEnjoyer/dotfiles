db98a76c167b79ee1d45e39fc94d87f9461d6dd5
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-06 21:17:09 +0200

    [UPDATE] repo.sh
    [EDIT] scripts path

--- scripts/setup/repo.sh
+++ scripts/setup/repo.sh
@@ -6,11 +6,11 @@ FORMAT=$(gum choose "HTTPS" "SSH" "ZIP" "Cancel")
 
 if [ "$FORMAT" == "HTTPS" ]; then
   git clone https://github.com/YetAnotherMechanicusEnjoyer/template.git ~/template
-  git clone https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater.git ./scripts/discord
+  git clone https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/YAME/scripts/discord
 elif [ "$FORMAT" == "SSH" ]; then
   git clone git@github.com:YetAnotherMechanicusEnjoyer/template.git ~/template
-  git clone git@github.com:YetAnotherMechanicusEnjoyer/discord-autoupdater.git ./scripts/discord
+  git clone git@github.com:YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/YAME/scripts/discord
 elif [ "$FORMAT" == "ZIP" ]; then
-  sh "$PWD"/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/template/archive/refs/heads/main.zip" "$HOME/template"
-  sh "$PWD"/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater/archive/refs/heads/main.zip" "$PWD/scripts/discord"
+  sh "$HOME"/YAME/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/template/archive/refs/heads/main.zip" "$HOME/template"
+  sh "$HOME"/YAME/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater/archive/refs/heads/main.zip" "$HOME/YAME/scripts/discord"
 fi
