89cd12529ac364d75ce7ef9ae29eeaad0dfa0a44
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-06 21:54:39 +0200

    [FIX] link.sh
    [FIX] simlinks were backward

--- link.sh
+++ link.sh
@@ -4,16 +4,16 @@ echo -e "\n\e[1m\e[96mDo you want to link config files ?\e[0m"
 gum confirm && echo || exit
 
 echo -e "\e[1m\e[96mChoose a Neovim config.\e[0m"
-NVIM=$(gum choose "lazyvim" "nvchad" "old_nvim")
+NVIM=$(gum choose "lazyvim" "old_nvim")
 CONFIG="$HOME/.config"
 
 rm -rf "$CONFIG/fastfetch" "$CONFIG/ghostty" "$CONFIG/nvim" "$CONFIG/starship.toml"
 
 printf "\e[95m"
-ln -sv "./.zshrc" "$HOME/.zshrc"
-ln -sv "./.tmux.conf" "$HOME/.tmux.conf"
-ln -sv "$PWD/.config/fastfetch" "$CONFIG/fastfetch"
-ln -sv "$PWD/.config/ghostty/" "$CONFIG/ghostty"
-ln -sv "$PWD/.config/$NVIM" "$CONFIG/nvim"
-ln -sv "$PWD/.config/starship.toml" "$CONFIG/starship.toml"
+ln -svf "$HOME/YAME/.zshrc" "$HOME/.zshrc"
+ln -svf "$HOME/YAME/.tmux.conf" "$HOME/.tmux.conf"
+ln -svf "$HOME/YAME/config/fastfetch/" "$CONFIG/fastfetch"
+ln -svf "$HOME/YAME/config/ghostty/" "$CONFIG/ghostty"
+ln -svf "$HOME/YAME/config/$NVIM/" "$CONFIG/nvim"
+ln -svf "$HOME/YAME/config/starship.toml" "$CONFIG/starship.toml"
 printf "\e[0m"
