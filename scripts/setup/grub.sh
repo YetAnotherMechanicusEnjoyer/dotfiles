33bf36d9592bcb3a49cd3cd94ee02a6ebff3ba63
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-08 02:14:58 +0200

    [UPDATE] grub.sh
    [REMOVE] sh ~/.../choose_background.sh

--- scripts/setup/grub.sh
+++ scripts/setup/grub.sh
@@ -3,7 +3,6 @@
 set -e
 
 git clone https://github.com/Lxtharia/minegrub-theme.git "$HOME"/YAME/grub
-sh ~/YAME/grub/choose_background.sh
 cd ~/YAME/
 INSTALL="./grub/install_theme.sh"
 sudo sh "$INSTALL"
