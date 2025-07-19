bf24c868bf444d2b1013b19bd2b211d375cb2d14
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-20 00:21:39 +0200

    [UPDATE] grub.sh
    [ADD] grub-mkconfig command

--- scripts/setup/grub.sh
+++ scripts/setup/grub.sh
@@ -6,4 +6,5 @@ git clone https://github.com/Lxtharia/minegrub-theme.git "$HOME"/YAME/grub
 cd ~/YAME/
 INSTALL="./grub/install_theme.sh"
 sudo sh "$INSTALL"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 cd - >/dev/null
