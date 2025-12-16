eeaf122cd43fd82db47b7f6e50d43d48a809ad9f
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-12-16 16:54:33 +0100

    [FIX] link.sh

--- link.sh
+++ link.sh
@@ -6,18 +6,19 @@ gum confirm && echo || exit
 echo -e "\e[1m\e[96mChoose a Neovim config.\e[0m"
 NVIM=$(gum choose "lazyvim" "old_nvim")
 CONFIG="$HOME/.config"
+DOTFILES="$HOME/.yame"
 
 rm -rf "$CONFIG/fastfetch" "$CONFIG/ghostty" "$CONFIG/nvim" "$CONFIG/starship.toml"
 
-cp ./.zshrc ~/YAME/
-cp ./.tmux.conf ~/YAME/
+cp ./.zshrc ~/.yame/
+cp ./.tmux.conf ~/.yame/
 
 printf "\e[95m"
-ln -svf "$HOME/YAME/.zshrc" "$HOME/.zshrc"
-ln -svf "$HOME/YAME/.tmux.conf" "$HOME/.tmux.conf"
-ln -svf "$HOME/YAME/config/fastfetch/" "$CONFIG/fastfetch"
-ln -svf "$HOME/YAME/config/ghostty/" "$CONFIG/ghostty"
-ln -svf "$HOME/YAME/config/$NVIM/" "$CONFIG/nvim"
-ln -svf "$HOME/YAME/config/starship.toml" "$CONFIG/starship.toml"
-sudo ln -svf "$HOME/YAME/scripts/updatearch.sh" "/usr/bin/updatearch"
+ln -svf "$DOTFILES/.zshrc" "$HOME/.zshrc"
+ln -svf "$DOTFILES/.tmux.conf" "$HOME/.tmux.conf"
+ln -svf "$DOTFILES/config/fastfetch/" "$CONFIG/fastfetch"
+ln -svf "$DOTFILES/config/ghostty/" "$CONFIG/ghostty"
+ln -svf "$DOTFILES/config/$NVIM/" "$CONFIG/nvim"
+ln -svf "$DOTFILES/config/starship.toml" "$CONFIG/starship.toml"
+sudo ln -svf "$DOTFILES/scripts/updatearch.sh" "/usr/bin/updatearch"
 printf "\e[0m"
