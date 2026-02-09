9fda1bafcdc7eba8cf07ef66ccb53cf6a2297d4e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-09 06:52:26 +0100

    [FIX] exports.zsh

--- config/zshrc.d/exports.zsh
+++ config/zshrc.d/exports.zsh
@@ -1,4 +1,4 @@
-export DOTFILES_PATH="$HOME/my_dotfiles/scripts"
+export DOTFILES_PATH="$HOME/.yame"
 export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
 export PATH="$PATH:$HOME/.tmuxifier/bin:$HOME/scripts:$HOME/.cargo/bin:$HOME/scripts"
 export EDITOR="nvim"
