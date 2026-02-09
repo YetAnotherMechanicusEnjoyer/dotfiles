d23d0c34c7fd42a1116c425b40709c2434515a22
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-09 06:52:12 +0100

    [MOVE] load_tokens.zsh -> config/zshrc.d/load_tokens.zsh

--- config/zshrc.d/load_tokens.zsh
+++ config/zshrc.d/load_tokens.zsh
@@ -1,7 +1,7 @@
-TOKEN_FILE="$HOME/dotfiles/.tokens"
+TOKEN_FILE="$DOTFILES_PATH/.tokens"
 
 if [[ ! -f "$TOKEN_FILE" ]]; then
-  echo "Error: File $TOKEN_FILE not found." >>"$HOME/.yame/errors.log"
+  echo "Error: File $TOKEN_FILE not found." >>"$DOTFILES_PATH/errors.log"
   return 1 2>/dev/null
 fi
 
