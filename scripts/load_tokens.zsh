7af55cb390b7dc3b3c1d81fb506d4988a6165c4e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-09 06:27:44 +0100

    [RENAME] load_tokens.sh -> load_tokens.zsh

--- scripts/load_tokens.zsh
+++ scripts/load_tokens.zsh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 TOKEN_FILE="$HOME/dotfiles/.tokens"
 
 if [[ ! -f "$TOKEN_FILE" ]]; then
@@ -7,5 +5,8 @@ if [[ ! -f "$TOKEN_FILE" ]]; then
   return 1 2>/dev/null
 fi
 
-export DISCORD_TOKEN=$(sed -n '1p' "$TOKEN_FILE")
-export BOT_TOKEN=$(sed -n '2p' "$TOKEN_FILE")
+FIRST_TOKEN=$(sed -n '1p' "$TOKEN_FILE")
+SECOND_TOKEN=$(sed -n '2p' "$TOKEN_FILE")
+
+export DISCORD_TOKEN="$FIRST_TOKEN"
+export BOT_TOKEN="$SECOND_TOKEN"
