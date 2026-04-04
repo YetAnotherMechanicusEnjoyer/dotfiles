82add04cac74c62659f0f5c5e901a0b87c16743e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-04-04 11:14:59 +0200

    [FIX] sddm location

--- scripts/setup/sddm.sh
+++ scripts/setup/sddm.sh
@@ -4,6 +4,6 @@ set -e
 
 git clone https://github.com/Davi-S/sddm-theme-minesddm.git "$HOME"/.yame/sddm
 cd ~/.yame/
-sudo cp -r "$HOME"/sddm/minesddm /usr/share/sddm/themes/
+sudo cp -r ./sddm/minesddm /usr/share/sddm/themes/
 sudo sed -i '/^\[Theme\]/,/^\[/ s/^Current=.*/minesddm/' /usr/lib/sddm/sddm.conf.d/default.conf
 cd - >/dev/null
