f786728651f1d9ed869ded41f7b2b003995efa0e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-04-04 07:57:38 +0200

    [ADD] end-4 dotfiles

--- setup.sh
+++ setup.sh
@@ -18,7 +18,13 @@ gum confirm && sh ~/.yame/scripts/setup/repo.sh
 echo -e "\n\e[1m\e[96mDo you want to install Lxtharia's minegrub theme ?\e[0m"
 gum confirm && sh ~/.yame/scripts/setup/grub.sh
 
-echo -e "\n\e[1m\e[96mDo you want to install ML4W's hyprland dotfiles ?\e[0m"
-gum confirm && sh ~/.yame/scripts/setup/hyprland_install.sh
+echo -e "\n\e[1m\e[96mDo you want to install other dotfiles ?\e[0m"
+action=$(gum choose "ML4W" "end-4" "no")
+
+if [[ $action == "ML4W" ]]; then
+  sh ~/.yame/scripts/setup/ml4w_install.sh
+elif [[ $action == "end-4" ]]; then
+  bash <(curl -s https://ii.clsty.link/get)
+fi
 
 sh ~/.yame/link.sh
