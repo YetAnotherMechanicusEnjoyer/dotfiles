75530505dd4cb4119212bf0c2b5fbc5c99e7e7af
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-04-04 11:12:54 +0200

    [UPDATE] os-prober in grub.sh & .yame/ instead of YAME/

--- scripts/setup/grub.sh
+++ scripts/setup/grub.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-git clone https://github.com/Lxtharia/double-minegrub-menu.git "$HOME"/YAME/grub
-cd ~/YAME/
+git clone https://github.com/Lxtharia/double-minegrub-menu.git "$HOME"/.yame/grub
+cd ~/.yame/
 INSTALL="./grub/install.sh"
 sudo sh "$INSTALL"
+sudo os-prober
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 cd - >/dev/null
--- scripts/setup/repo.sh
+++ scripts/setup/repo.sh
@@ -4,11 +4,11 @@ FORMAT=$(gum choose "HTTPS" "SSH" "ZIP" "Cancel")
 
 if [ "$FORMAT" == "HTTPS" ]; then
   git clone https://github.com/YetAnotherMechanicusEnjoyer/template.git ~/template
-  git clone https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/YAME/scripts/discord
+  git clone https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/.yame/scripts/discord
 elif [ "$FORMAT" == "SSH" ]; then
   git clone git@github.com:YetAnotherMechanicusEnjoyer/template.git ~/template
-  git clone git@github.com:YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/YAME/scripts/discord
+  git clone git@github.com:YetAnotherMechanicusEnjoyer/discord-autoupdater.git ~/.yame/scripts/discord
 elif [ "$FORMAT" == "ZIP" ]; then
-  sh "$HOME"/YAME/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/template/archive/refs/heads/main.zip" "$HOME/template"
-  sh "$HOME"/YAME/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater/archive/refs/heads/main.zip" "$HOME/YAME/scripts/discord"
+  sh "$HOME"/.yame/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/template/archive/refs/heads/main.zip" "$HOME/template"
+  sh "$HOME"/.yame/scripts/download_zip.sh "https://github.com/YetAnotherMechanicusEnjoyer/discord-autoupdater/archive/refs/heads/main.zip" "$HOME/YAME/scripts/discord"
 fi
