d65b69b914377973f56e66da7cfc4856634d3c84
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-07-26 20:21:52 +0200

    fix: config/zshrc.d

--- config/zshrc.d/exec.zsh
+++ config/zshrc.d/exec.zsh
@@ -1,1 +1,6 @@
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+  mkdir -p ~/.cache
+  exec Hyprland > ~/.cache/hyprland.log 2>&1
+fi
+
 fastfetch
--- config/zshrc.d/zinit.zsh
+++ config/zshrc.d/zinit.zsh
@@ -15,11 +15,6 @@ zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
 
 zinit snippet OMZP::sudo
-zinit snippet OMZP::archlinux
-zinit snippet OMZP::aws
-zinit snippet OMZP::kubectl
-zinit snippet OMZP::kubectx
-zinit snippet OMZP::command-not-found
 
 zinit cdreplay -q
 
