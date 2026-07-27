52f3f1ba76b6bd055cb47433b061ea4a786cb4d9
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-07-27 21:11:54 +0200

    feat: add an env variable to skip exec.zsh

--- config/zshrc.d/exec.zsh
+++ config/zshrc.d/exec.zsh
@@ -1,6 +1,8 @@
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
-  mkdir -p ~/.cache
-  exec Hyprland > ~/.cache/hyprland.log 2>&1
-fi
+if [[ -z "$SKIP_EXEC_ZSH" ]]; then
+  if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+    mkdir -p ~/.cache
+    exec Hyprland > ~/.cache/hyprland.log 2>&1
+  fi
 
 fastfetch
+fi
