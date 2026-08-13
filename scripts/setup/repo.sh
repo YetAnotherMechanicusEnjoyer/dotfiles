806b75347280bbdcbc76b6ada69587c6be1b797f
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-13 20:25:09 +0200

    fix: repositories build & update

--- scripts/setup/repo.sh
+++ scripts/setup/repo.sh
@@ -33,10 +33,20 @@ for entry in "${REPOSITORIES[@]}"; do
     elif [ "$FORMAT" == "ZIP" ]; then
       sh "$DOTFILES/scripts/download_zip.sh" "https://github.com/$AUTHOR/$REPO/archive/refs/heads/main.zip" "$REPODIR/$REPO"
     fi
+
+    if cd "$REPODIR/$REPO" >&/dev/null; then
+      [[ -f "$REPODIR/$REPO/build.zig" && "$REPO" != *"template"* ]] && zig build -j"$(nproc)"
+      [[ -f "$REPODIR/$REPO/Cargo.toml" && "$REPO" != *"template"* ]] && cargo build -j "$(nproc)" --release
+      [[ -f "$REPODIR/$REPO/Makefile" && "$REPO" != *"template"* ]] && make -j "$(nproc)"
+      [[ -f "$REPODIR/$REPO/CMakeLists.txt" && "$REPO" != *"template"* ]] && cmake -S . -B build && cmake --build build --parallel "$(nproc)"
+    else
+      echo -e "\e[90m:: \e[1;91mERROR\e[0;90m - Cannot cd to \e[1;3;94m$REPODIR/$REPO\e[0;90m\e[0m" &&
+        echo -e "\e[0;90m:: Press \e[1;94m[ENTER]\e[0;90m to close.\e[0m" &&
+        read &&
+        continue
+    fi
+
+    cd - >&/dev/null || echo -e "\e[90m:: \e[1;91mERROR\e[0;90m - Cannot cd back\e[0m"
+    echo
   fi
 done
-
-cd "$REPODIR/herma" && zig build
-cd - >/dev/null
-cd "$REPODIR/wayra" && zig build
-cd - >/dev/null
