b06160a6a80e3ba42fbac5421215f30ea7fb97aa
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-17 00:45:02 +0200

    feat: add i3 config link

--- scripts/setup/link.sh
+++ scripts/setup/link.sh
@@ -3,7 +3,7 @@
 NVIM="lazyvim"
 CONFIG="$HOME/.config"
 
-rm -rf "$CONFIG/fastfetch" "$CONFIG/ghostty" "$CONFIG/nvim" "$CONFIG/starship.toml" "$CONFIG/zshrc.d" "$CONFIG/hypr/custom"
+rm -rf "$CONFIG/fastfetch" "$CONFIG/ghostty" "$CONFIG/nvim" "$CONFIG/starship.toml" "$CONFIG/zshrc.d" "$CONFIG/hypr/custom" "$CONFIG/i3"
 
 printf "\e[1;3;94m"
 ln -svf "$DOTFILES/.zshrc" "$HOME/.zshrc"
@@ -14,6 +14,7 @@ ln -svf "$DOTFILES/config/$NVIM/" "$CONFIG/nvim"
 ln -svf "$DOTFILES/config/starship.toml" "$CONFIG/starship.toml"
 ln -svf "$DOTFILES/config/zshrc.d/" "$CONFIG/zshrc.d"
 ln -svf "$DOTFILES/config/hypr/custom" "$CONFIG/hypr/custom"
+ln -svf "$DOTFILES/config/i3" "$CONFIG/i3"
 sudo ln -svf "$DOTFILES/scripts/download_zip" "/usr/bin/download_zip"
 sudo ln -svf "$DOTFILES/scripts/extract" "/usr/bin/extract"
 sudo ln -svf "$DOTFILES/scripts/updatearch" "/usr/bin/updatearch"
