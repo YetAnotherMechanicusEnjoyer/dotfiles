04001fb18ef0bc246ded7cb9d74aaabdfc1cb74c
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-17 16:33:40 +0200

    fix: default link variables

--- scripts/setup/link.sh
+++ scripts/setup/link.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 NVIM="lazyvim"
-CONFIG="$HOME/.config"
+CONFIG="${XDG_CONFIG_HOME:-${HOME}/.config}"
+DOTFILES="${DOTFILES:-${HOME}/.yame}"
+REPODIR="${REPODIR:-${HOME}/.yamerepo}"
 
 rm -rf "$CONFIG/fastfetch" "$CONFIG/ghostty" "$CONFIG/nvim" "$CONFIG/starship.toml" "$CONFIG/zshrc.d" "$CONFIG/hypr/custom" "$CONFIG/i3"
 
@@ -13,12 +15,12 @@ ln -svf "$DOTFILES/config/ghostty/" "$CONFIG/ghostty"
 ln -svf "$DOTFILES/config/$NVIM/" "$CONFIG/nvim"
 ln -svf "$DOTFILES/config/starship.toml" "$CONFIG/starship.toml"
 ln -svf "$DOTFILES/config/zshrc.d/" "$CONFIG/zshrc.d"
-ln -svf "$DOTFILES/config/hypr/custom" "$CONFIG/hypr/custom"
 ln -svf "$DOTFILES/config/i3" "$CONFIG/i3"
 sudo ln -svf "$DOTFILES/scripts/download_zip" "/usr/bin/download_zip"
 sudo ln -svf "$DOTFILES/scripts/extract" "/usr/bin/extract"
 sudo ln -svf "$DOTFILES/scripts/updatearch" "/usr/bin/updatearch"
 
+[[ -d "$CONFIG/hypr" ]] && ln -svf "$DOTFILES/config/hypr/custom" "$CONFIG/hypr/custom"
 [[ -f "$REPODIR/wayra/zig-out/bin/herma" ]] && sudo ln -svf "$REPODIR/wayra/zig-out/bin/herma" "/usr/bin/herma"
 [[ -f "$REPODIR/wayra/zig-out/bin/wayra" ]] && sudo ln -svf "$REPODIR/wayra/zig-out/bin/wayra" "/usr/bin/wayra"
 [[ -f "$REPODIR/vimcord/target/release/vimcord" ]] && sudo ln -svf "$REPODIR/vimcord/target/release/vimcord" "/usr/bin/vimcord"
