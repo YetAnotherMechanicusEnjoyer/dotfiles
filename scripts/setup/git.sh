1960ede253a6fce847ca2014329f7323c9d2177a
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-18 21:37:11 +0200

    remove: ssh generation in git.sh

--- scripts/setup/git.sh
+++ scripts/setup/git.sh
@@ -7,14 +7,3 @@ username=$(gum input --placeholder "Enter your github username")
 
 git config --global user.email "$email"
 git config --global user.name "$username"
-
-echo -e "\n\e[0;90mDo you want to generate an ssh key ?\e[0m"
-gum confirm && echo || exit
-ssh-keygen -t rsa -b 4096 -C "$email"
-echo
-
-cat ~/.ssh/id_rsa.pub
-
-echo -e "\n\e[0;90mThis is your \e[3;94mpublic key\e[0;90m, you can copy it.\e[0m"
-echo -e "\e[0;90m:: Press \e[1;94m[ENTER]\e[0;90m to continue.\e[0m"
-read
